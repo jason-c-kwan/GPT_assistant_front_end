90520207d5cb70ce6486dfc2b67c4ad2287f8d1d
Jason Kwan <jason.kwan@wisc.edu>
2023-11-22 21:52:03 -0600

    Updated so that it works with an existing assistant

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI  # Import OpenAI
 from config import OPENAI_API_KEY
+from config import ASSISTANT_ID
+import time
 
 app = Flask(__name__)
 
 # Initialize OpenAI client with your API key
 client = OpenAI(api_key=OPENAI_API_KEY)
+assistant = client.beta.assistants.retrieve(ASSISTANT_ID)
+thread = client.beta.threads.create()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Get the assistant's name and pass it to the template
+    assistant_name = assistant.name
+    return render_template('index.html', assistant_name=assistant_name)
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -17,14 +23,36 @@ def send_message():
     user_message = data['message']
     
     try:
-        chat_completion = client.chat.completions.create(
-            messages=[{"role": "user", "content": user_message}],
-            model="gpt-4-1106-preview"
+        message = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=user_message
         )
 
-        # Adjust this line to correctly access the response
-        # Example: reply = chat_completion.get_response()
-        reply = chat_completion.choices[0].message.content  # Adjusted line
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant.id
+        )
+
+        # Poll every 5 seconds in order to see if run has completed
+        completed = False
+        failed = False
+        while completed == False:
+            time.sleep(5)
+            run_steps = client.beta.threads.runs.steps.list(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            current_status = run_steps.data[-1].status
+            if current_status == 'completed':
+                completed = True
+                failed = False
+            elif current_status == 'expired' or current_status == 'cancelled' or current_status == 'failed':
+                completed = True
+                failed = True
+        
+        thread_messages = client.beta.threads.messages.list(thread.id)
+        reply = thread_messages.data[0].content[0].text.value
 
         return jsonify({'reply': reply, 'error': None})
     except Exception as e:  # Catch any exception
